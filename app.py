700cc88d7ea19870075b26c07b693f4889de43ef
AbdouKaled <74584964+gakas14@users.noreply.github.com>
2022-03-20 14:20:39 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ st.set_page_config('wide')
 # Create sidebar
 st.sidebar.markdown("<div> <img src='https://content.sportslogos.net/news/2021/07/nba-75th-anniversary-2021-2022-wilt-kareem-jordan-kobe-sportslogosnet-logo-1170x807.png' width=300/>  <h1 style='display:inline-block'> NBA Players Stats Dash </h1> </div>", unsafe_allow_html=True)
 st.sidebar.markdown("This dashboard allows you to search for a particular NBA player.")
-st.sidebar.markdown("You GET <ol><li>You can search a particular player.</li> <li>You can see the current season stats of the player.</li> "
+st.sidebar.markdown("You GET <ol><li>You can search for a particular player.</li> "
+                    "<li>You can see the current season stats of the player..</li> "
                     "<li>A chart of the players shooting Area, different area shots attempted and made.</li>"
-                    "<li>A chart of th player average PTS,AST and REB over years.</li>"
-                    "<li>The player total numbers in the Regular Season, PlayOff and All-Star game.</li>"
+                    "<li>A chart of the player average PTS, AST, and REB over years.</li>"
+                    "<li>The player total numbers in the Regular Season, Playoff, and All-Star game.</li>"
                     "</ol>", unsafe_allow_html=True)
 
 
